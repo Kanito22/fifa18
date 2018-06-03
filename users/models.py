039e4aa13fff6797393a0f49b4f09c264bb4691f
Joel Andersson <joelandersson33@gmail.com>
2018-06-03 20:45:43 +0200

    select bet on winning team during signup

--- users/models.py
+++ users/models.py
@@ -1,10 +1,12 @@
 # users/models.py
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from polls.models import Team
 
 class CustomUser(AbstractUser):
     # First/last name is not a global-friendly pattern
     name = models.CharField(blank=True, max_length=255)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return self.email
